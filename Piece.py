995e6dce19acad7a901e1dfe3499e9ebe3edc662
Gou.Hattori <hattori@kanaban.com>
2019-02-07 21:50:23 +0900

    前のpieceからの値の継承方法追加(設定に書いた方がわかりやすいので) descriptionをdetailsの冒頭に追加 header = 見出し details = ユーザー用のメモ logger = 開発者用のログ

--- Piece.py
+++ Piece.py
@@ -6,18 +6,32 @@ from . import PzLog
 class Piece(object):
     def __init__(self, **args):
         #self.data = args["data"]
-        self.data = copy.deepcopy(args["data"])
-        self.pass_data = args["pass_data"]
-        self.piece_data = args["piece_data"]
-        self.logger = args["logger"]
+        self.data = copy.deepcopy(args.get("data", {}))
+        self.pass_data = args.get("pass_data", {})
+        self.piece_data = args.get("piece_data", {})
+        self.logger = args.get("logger", False)
+        if not self.logger:
+            log = PzLog.PzLog()
+            self.logger = log.logger
         
         if "paint" in self.piece_data:
+            """
+            startwith @: check it from pass_data
+            """
             for k, v in self.piece_data["paint"].items():
-                if v in self.data:
+                if v.startswith("@"):
+                    if v[1:] in self.pass_data:
+                        self.data[k] = self.pass_data[v[1:]]
+
+                elif v in self.data:
                     self.data[k] = self.data[v]
-                    del self.data[v]
+                    # del self.data[v]
         self.header = ""
-        self.detail = ""
+        description = self.piece_data.get("description", "")
+        self.details = []
+        if description != u"":
+            self.details = ["", u"【{}】".format(description)]
+
         self.filtered = True
         if "filters" in self.piece_data:
             for filter_ in self.piece_data["filters"]:
@@ -33,4 +47,4 @@ class Piece(object):
                             self.filtered = False
 
     def execute(self, **args):
-        return True, self.pass_data, self.header, self.detail
+        return True, self.pass_data, self.header, self.details
